15a8d46a126b4c91d8b1e010c597491bf4c377d2
comet13579 <u3622245@connect.hku.hk>
2024-09-11 13:00:30 +0800

    fix ackermann

--- ackermann.c
+++ ackermann.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
-#define m 1
-#define n 1
+#define m 3
+#define n 4
 
 int Ackermann(int a,int b){
-    printf("a = %d b = %d\n",a,b);
-    if (a == 0 && b >= 0){
+    if (a == 0){
         return b + 1;
     }
-    else if (b == 0 && a > 0){
-        Ackermann(a - 1,1);
+    else if (b == 0){
+        return Ackermann(a - 1,1);
     }
-    else if (a > 0 && b > 0){
-        Ackermann(a - 1,Ackermann(a,b-1));
+    else{
+        return Ackermann(a - 1,Ackermann(a,b-1));
     }
-    return 0;
 }
 
 
 int main(){
-    Ackermann(1,1);
+    printf("ackermann: %d \n",Ackermann(m,n));
     return 0;
 }
