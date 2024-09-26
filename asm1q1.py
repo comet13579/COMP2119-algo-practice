e20d2a40355ec2a7532a6c32056fac007e325ec8
Comet <u3622245@connect.hku.hk>
2024-09-27 00:38:21 +0900

    asm1q1.py

---
+++ asm1q1.py
@@ -0,0 +1,23 @@
+import math
+
+n = 100 ## 100 growth
+
+print("a:", math.pow(n, math.pi))
+print("b:", math.pow(math.pi, n))
+print("c:", math.pow(n, n))
+print("d:", math.log(n, 2))
+print("e:", math.pow(math.pi, math.log(n, 2)))
+print("f:", math.pow(n, math.log(math.pi, 2)))
+print("g:", n / math.log(math.pi, 2))
+print("h:", n / math.log(n, 2))
+print("i:", n / math.log(math.log(n, 2), 2))
+print("j:", math.log(n / math.log(n, 2), 2))
+print("k:", math.pow(math.pi,math.log(2 * n, 2)))
+print("l:", math.pow(n,math.log(2 * math.pi, 2)))
+
+msum = 0
+for i in range(1,n+1):
+    msum += (i+1)
+print("m:", math.pow(msum,0.5))
+
+print("n:", 1910 * math.factorial(n) + 316 * math.pow(n,n))
