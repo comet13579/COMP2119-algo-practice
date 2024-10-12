c6f6f50fec96ef2a5dc926a37dc625667e7947fe
Comet <u3622245@connect.hku.hk>
2024-10-13 00:38:42 +0900

    asm2q4.py

---
+++ asm2q4.py
@@ -0,0 +1,44 @@
+hashtable = [17,94,86,22,98,79,54,78]
+
+newhashtable = []
+totalI = 0
+for i in hashtable:
+    totalI += newhashtable.count(i % 7)
+    newhashtable.append(i % 7)
+print(hashtable)
+print(newhashtable)
+print("total unsuccessful search:", totalI)
+print()
+#q4a
+
+newhashtable = []
+totalI = 0
+for i in hashtable:
+    newhash = i % 11
+    newI = 1
+    while newhash in newhashtable:
+        newhash = (newhash + 3 * newI) % 11
+        newI += 1
+    totalI += newI - 1
+    newhashtable.append(newhash)
+print(hashtable)
+print(newhashtable)
+print("total unsuccessful search:", totalI)
+print()
+#q4b
+
+newhashtable = []
+totalI = 0
+for i in hashtable:
+    newhash = i % 11
+    newI = 1
+    while newhash in newhashtable:
+        newhash = (i % 11 + (2 - i % 2) * newI) % 11
+        newI += 1
+    totalI += newI - 1
+    newhashtable.append(newhash)
+
+print(hashtable)
+print(newhashtable)
+print("total unsuccessful search:", totalI)
+#q4c
