b0d2f01c700462bf2043922cdb1cecd70732fc53
comet13579 <u3622245@connect.hku.hk>
2024-10-21 13:39:31 +0800

    reorganizing asm2q4.py

--- asm2q4.py
+++ asm2q4.py
@@ -7,41 +7,65 @@ for i in hashtable:
     newhashtable.append(i % 7)
 print(hashtable)
 print(newhashtable)
-print("total unsuccessful search:", totalI)
-print()
+print(f"Q4b: average search till uncessessful search: {(7 + totalI) /7}")
 #q4a
 
-newhashtable = []
-totalI = 0
+def q2hash(hash,i):
+    return (hash + 3 * i) % 11
+
+hashtablevalues = []
+probinghash = [None] * 11
+count = 0
 for i in hashtable:
     newhash = i % 11
     newI = 1
-    while newhash in newhashtable:
-        newhash = (newhash + 3 * newI) % 11
+    while newhash in hashtablevalues:
+        newhash = q2hash(i,newI)
         newI += 1
-    totalI += newI - 1
-    print(newI - 1, end=" ")
-    newhashtable.append(newhash)
-print()
-print(hashtable)
-print(newhashtable)
-print("total unsuccessful search:", totalI)
-print()
+    hashtablevalues.append(newhash)
+
+for i in hashtablevalues:
+    probinghash[i] = hashtable[hashtablevalues.index(i)]
+print(probinghash)
+
+for i in range(0,11):
+    newI = 1
+    count += 1
+    newhash = i
+    while probinghash[newhash] != None:
+        newhash = q2hash(i,newI)
+        newI += 1
+        count += 1
+print(f"Q4b: total searches: {count}")
+print(f"Q4b: average search till uncessessful search: {count/11}")
 #q4b
 
-newhashtable = []
-totalI = 0
+def q3hash(hash,i):
+    return (hash % 11 + i * (2 - i % 2)) % 11
+
+hashtablevalues = []
+probinghash = [None] * 11
+count = 0
 for i in hashtable:
     newhash = i % 11
     newI = 1
-    while newhash in newhashtable:
-        newhash = (i % 11 + (2 - i % 2) * newI) % 11
+    while newhash in hashtablevalues:
+        newhash = q3hash(i,newI)
         newI += 1
-    totalI += newI - 1
-    newhashtable.append(newhash)
-    print(newI - 1, end=" ")
-print()
-print(hashtable)
-print(newhashtable)
-print("total unsuccessful search:", totalI)
+    hashtablevalues.append(newhash)
+
+for i in hashtablevalues:
+    probinghash[i] = hashtable[hashtablevalues.index(i)]
+print(probinghash)
+
+for i in range(0,11):
+    newI = 1
+    count += 1
+    newhash = i
+    while probinghash[newhash] != None:
+        newhash = q3hash(i,newI)
+        newI += 1
+        count += 1
+print(f"Q4c: total searches: {count}")
+print(f"Q4c: average search till uncessessful search: {count/11}")
 #q4c
